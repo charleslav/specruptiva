fd67f61b93d622640f3c794a8f1a383b6974f882
Jérémy Viau-Trudel <jeremy.viau-trudel@noumanity.com>
2025-07-18 00:23:07 -0400

    [wasm] fournit la fonction validate

--- cmd/wasm/main.go
+++ cmd/wasm/main.go
@@ -1,9 +1,50 @@
 package main
 
 import (
+	"syscall/js"
 	"fmt"
+	"disruptiva.org/specruptiva/pkg/core/service"
+	"disruptiva.org/specruptiva/adapters/cue"
 )
+
+
+var (
+	validator service.ValidateService
+	schema string
+	data string
+)
+
+
+
+func jsonWrapper() js.Func {
+	jsonFunc := js.FuncOf(func(this js.Value, args []js.Value) any {
+        
+    if len(args) !=2 {
+			return "2 arguments sont requis. (SCHEMA , DATA)"
+		}
+    // todo: mettre tout ça dans une fonciton à part
+		validator:= cue.NewValidator(cue.ValidateFromString)
+    vs:=service.NewValidateService(validator)
+
+    schema=args[0].String()
+    data=args[1].String()
+		fmt.Println("schema: ", schema)
+		fmt.Println("data: ", data)
+
+  	vs.SetSchema(schema)
+  	vs.SetData(data)
+  	err:= vs.Validate()
+  	if err !=nil {
+  		return err.Error()
+  	}
+		return 0
+
+	})
+	return jsonFunc
+}
 
 func main(){
 	fmt.Println("hello from go wasm")
+  js.Global().Set("validate", jsonWrapper())
+	<-make(chan struct{})
 }
